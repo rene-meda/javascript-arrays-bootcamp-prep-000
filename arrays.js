d19530643937b281abb31a5139e95b28b113d4d9
Adolpho Noguera <adolph518@gmail.com>
2018-03-24 18:38:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -43,7 +43,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  var newArray = array.slice()
+  var newArray = array.shift()
   return newArray
 }
 
