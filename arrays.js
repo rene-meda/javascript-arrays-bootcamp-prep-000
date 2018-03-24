bdeb98185b141a7b158ca862bf85607800d9a7a0
Adolpho Noguera <adolph518@gmail.com>
2018-03-24 18:15:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ function addElementToEndOfArray(array, element){
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  
+  return array.push(element)
 }
 
 function accessElementInArray(array, n){
