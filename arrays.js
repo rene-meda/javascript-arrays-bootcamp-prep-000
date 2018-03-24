10e30811f25653d84a133e99365ad713ee6af5a5
Adolpho Noguera <adolph518@gmail.com>
2018-03-24 18:28:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,7 +21,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element){
-  newArray = [aray, ...element]
+  newArray = [array, ...element]
   return newArray
 }
 
